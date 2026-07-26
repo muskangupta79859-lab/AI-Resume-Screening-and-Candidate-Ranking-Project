33a03e105193a0c786967fd3f1def47776214600
muskangupta79859-lab <muskangupta79859@gmail.com>
2026-07-26 18:06:16 +0530

    Remove spaCy dependency

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,36 +1,26 @@
 import re
 import nltk
-import spacy
 from nltk.corpus import stopwords
 
-# Download NLTK stopwords if missing
+# Download stopwords if missing
 try:
     stop_words = set(stopwords.words("english"))
 except LookupError:
     nltk.download("stopwords")
     stop_words = set(stopwords.words("english"))
 
-# Load spaCy model (download automatically if missing)
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    from spacy.cli import download
-
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
-
 
 def clean_text(text):
     text = text.lower()
     text = re.sub(r"\d+", "", text)
-    text = re.sub(r"[^\w\s]", "", text)
+    text = re.sub(r"[^\w\s]", " ", text)
 
-    doc = nlp(text)
+    words = text.split()
 
-    cleaned = []
-
-    for token in doc:
-        if token.text not in stop_words and not token.is_space:
-            cleaned.append(token.lemma_)
+    cleaned = [
+        word
+        for word in words
+        if word not in stop_words and len(word) > 1
+    ]
 
     return " ".join(cleaned)
